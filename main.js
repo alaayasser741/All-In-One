7e6136d6ded1ac94b936bf61fe1ee1ec7792aa1d
Alaa Yasser <74079336+alaayasser17@users.noreply.github.com>
2022-07-07 18:53:19 +0200

    add the project

---
+++ main.js
@@ -0,0 +1,15 @@
+let logo = document.querySelector(".logo");
+let social = document.querySelector(".social");
+
+// function showSocial(){
+//     if (social.style.display === "block") {
+//         social.style.display = "none";
+//     } else {
+//         social.style.display = "block";
+//     }
+// }
+
+
+
+
+
